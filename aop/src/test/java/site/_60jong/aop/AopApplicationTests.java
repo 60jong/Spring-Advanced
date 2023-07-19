7301ca088b02a278d7bb8e1680c8855c5c0bcb18
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-07-19 14:31:32 +0900

    setup

---
+++ aop/src/test/java/site/_60jong/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package site._60jong.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
